72d0e1aaf68df205433e3041a02360525cfe43c2
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2025-02-27 14:22:17 +0100

    Added users model

---
+++ EindOpdracht.WebApi/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace EindOpdracht.WebApi
+{
+    public class Users
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
